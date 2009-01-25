bae9569cbabeef49b6b1f8ee9ad0f14925d4ee4f
matt <matt@d5caad04-189b-467d-8c69-358c65116b6f>
2009-01-25 11:07:28 +0000

    ws change
    git-svn-id: http://www/svn/games/balls/trunk@238 d5caad04-189b-467d-8c69-358c65116b6f

--- net.todd.games.balls.client/src/main/java/net/todd/games/balls/client/ui/j3d/MainViewIn3D.java
+++ net.todd.games.balls.client/src/main/java/net/todd/games/balls/client/ui/j3d/MainViewIn3D.java
@@ -33,121 +33,121 @@ import com.sun.j3d.utils.universe.SimpleUniverse;
 import com.sun.j3d.utils.universe.ViewingPlatform;
 
 public class MainViewIn3D extends MainView implements IMainView {
-    private Bounds bounds;
+	private Bounds bounds;
 
-    @Override
-    public JPanel createMainPanel() {
-	GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
-	Canvas3D canvas3D = new Canvas3D(config);
-	canvas3D.setFocusable(true);
-	canvas3D.requestFocus();
+	@Override
+	public JPanel createMainPanel() {
+		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+		Canvas3D canvas3D = new Canvas3D(config);
+		canvas3D.setFocusable(true);
+		canvas3D.requestFocus();
 
-	SimpleUniverse su = new SimpleUniverse(canvas3D);
-	su.getViewingPlatform().setNominalViewingTransform();
-	su.getViewer().getView().setMinimumFrameCycleTime(5);
+		SimpleUniverse su = new SimpleUniverse(canvas3D);
+		su.getViewingPlatform().setNominalViewingTransform();
+		su.getViewer().getView().setMinimumFrameCycleTime(5);
 
-	bounds = new BoundingSphere(new Point3d(0, 0, 0), 100);
+		bounds = new BoundingSphere(new Point3d(0, 0, 0), 100);
 
-	su.addBranchGraph(createScene(su));
+		su.addBranchGraph(createScene());
 
-	createCamera(su);
+		createCamera(su);
 
-	JPanel mainPanel = new JPanel();
-	mainPanel.setLayout(new BorderLayout());
-	mainPanel.add(canvas3D, BorderLayout.CENTER);
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		mainPanel.add(canvas3D, BorderLayout.CENTER);
 
-	return mainPanel;
-    }
-
-    private BranchGroup createScene(SimpleUniverse su) {
-	BranchGroup bg = new BranchGroup();
-
-	lightScene(bg);
-	createGameGrid(bg);
-	createBall(bg);
-
-	return bg;
-    }
-
-    private void createGameGrid(BranchGroup bg) {
-	Shape3D grid = new Shape3D();
-
-	Point3f[] points = new Point3f[80];
-	int index = 0;
-	for (int i = -10; i < 10; i++) {
-	    float zPos = i;
-	    points[index * 2] = new Point3f(-10.0f, 0.0f, zPos);
-	    points[index * 2 + 1] = new Point3f(10.0f, 0.0f, zPos);
-	    index++;
+		return mainPanel;
 	}
 
-	index = 20;
-	for (int i = -10; i < 10; i++) {
-	    float xPos = i;
-	    points[index * 2] = new Point3f(xPos, 0.0f, -10.0f);
-	    points[index * 2 + 1] = new Point3f(xPos, 0.0f, 10.0f);
-	    index++;
+	private BranchGroup createScene() {
+		BranchGroup bg = new BranchGroup();
+
+		lightScene(bg);
+		createGameGrid(bg);
+		createBall(bg);
+
+		return bg;
 	}
 
-	LineArray gridGeometry = new LineArray(points.length, GeometryArray.COORDINATES
-	        | GeometryArray.COLOR_3);
-	gridGeometry.setCoordinates(0, points);
+	private void createGameGrid(BranchGroup bg) {
+		Shape3D grid = new Shape3D();
 
-	for (int i = 0; i < points.length; i++) {
-	    gridGeometry.setColor(i, new Color3f(1.0f, 1.0f, 1.0f));
+		Point3f[] points = new Point3f[80];
+		int index = 0;
+		for (int i = -10; i < 10; i++) {
+			float zPos = i;
+			points[index * 2] = new Point3f(-10.0f, 0.0f, zPos);
+			points[index * 2 + 1] = new Point3f(10.0f, 0.0f, zPos);
+			index++;
+		}
+
+		index = 20;
+		for (int i = -10; i < 10; i++) {
+			float xPos = i;
+			points[index * 2] = new Point3f(xPos, 0.0f, -10.0f);
+			points[index * 2 + 1] = new Point3f(xPos, 0.0f, 10.0f);
+			index++;
+		}
+
+		LineArray gridGeometry = new LineArray(points.length, GeometryArray.COORDINATES
+		        | GeometryArray.COLOR_3);
+		gridGeometry.setCoordinates(0, points);
+
+		for (int i = 0; i < points.length; i++) {
+			gridGeometry.setColor(i, new Color3f(1.0f, 1.0f, 1.0f));
+		}
+
+		Appearance gridAppearance = new Appearance();
+		LineAttributes lineAttributes = new LineAttributes();
+		lineAttributes.setLineWidth(1.0f);
+		gridAppearance.setLineAttributes(lineAttributes);
+
+		grid.setGeometry(gridGeometry);
+		grid.setAppearance(gridAppearance);
+
+		bg.addChild(grid);
 	}
 
-	Appearance gridAppearance = new Appearance();
-	LineAttributes lineAttributes = new LineAttributes();
-	lineAttributes.setLineWidth(1.0f);
-	gridAppearance.setLineAttributes(lineAttributes);
+	private void createCamera(SimpleUniverse su) {
+		ViewingPlatform vp = su.getViewingPlatform();
+		TransformGroup tg = vp.getViewPlatformTransform();
+		Transform3D t3d = new Transform3D();
+		tg.getTransform(t3d);
+		t3d.lookAt(new Point3d(10.0, 10.0, 10.0), new Point3d(0.0, 0.0, 0.0),
+		        new Vector3d(0, 1, 0));
+		t3d.invert();
+		tg.setTransform(t3d);
+	}
 
-	grid.setGeometry(gridGeometry);
-	grid.setAppearance(gridAppearance);
+	private void createBall(BranchGroup bg) {
+		Color3f blue = new Color3f(0.0f, 0.0f, 0.0f);
+		Color3f black = new Color3f(0.3f, 0.3f, 0.8f);
+		Color3f specular = new Color3f(0.9f, 0.9f, 0.9f);
 
-	bg.addChild(grid);
-    }
+		Material blueMat = new Material(blue, black, blue, specular, 25.0f);
+		Appearance blueApp = new Appearance();
+		blueApp.setMaterial(blueMat);
 
-    private void createCamera(SimpleUniverse su) {
-	ViewingPlatform vp = su.getViewingPlatform();
-	TransformGroup tg = vp.getViewPlatformTransform();
-	Transform3D t3d = new Transform3D();
-	tg.getTransform(t3d);
-	t3d.lookAt(new Point3d(10.0, 10.0, 10.0), new Point3d(0.0, 0.0, 0.0),
-	        new Vector3d(0, 1, 0));
-	t3d.invert();
-	tg.setTransform(t3d);
-    }
+		Transform3D t3d = new Transform3D();
+		t3d.set(new Vector3f(0, 0, 0));
+		TransformGroup tg = new TransformGroup(t3d);
+		tg.addChild(new Sphere(0.5f, 1, 50, blueApp));
 
-    private void createBall(BranchGroup bg) {
-	Color3f blue = new Color3f(0.0f, 0.0f, 0.0f);
-	Color3f black = new Color3f(0.3f, 0.3f, 0.8f);
-	Color3f specular = new Color3f(0.9f, 0.9f, 0.9f);
+		bg.addChild(tg);
+	}
 
-	Material blueMat = new Material(blue, black, blue, specular, 25.0f);
-	Appearance blueApp = new Appearance();
-	blueApp.setMaterial(blueMat);
+	private void lightScene(BranchGroup bg) {
+		Color3f white = new Color3f(1.0f, 1.0f, 1.0f);
+		AmbientLight ambient = new AmbientLight(white);
+		ambient.setInfluencingBounds(bounds);
+		bg.addChild(ambient);
 
-	Transform3D t3d = new Transform3D();
-	t3d.set(new Vector3f(0, 0, 0));
-	TransformGroup tg = new TransformGroup(t3d);
-	tg.addChild(new Sphere(0.5f, 1, 50, blueApp));
+		Vector3f direction1 = new Vector3f(-1.0f, -1.0f, -1.0f);
+		DirectionalLight direct1 = new DirectionalLight(white, direction1);
+		direct1.setInfluencingBounds(bounds);
+		bg.addChild(direct1);
+	}
 
-	bg.addChild(tg);
-    }
-
-    private void lightScene(BranchGroup bg) {
-	Color3f white = new Color3f(1.0f, 1.0f, 1.0f);
-	AmbientLight ambient = new AmbientLight(white);
-	ambient.setInfluencingBounds(bounds);
-	bg.addChild(ambient);
-
-	Vector3f direction1 = new Vector3f(-1.0f, -1.0f, -1.0f);
-	DirectionalLight direct1 = new DirectionalLight(white, direction1);
-	direct1.setInfluencingBounds(bounds);
-	bg.addChild(direct1);
-    }
-
-    public void setBallPositions(Ball[] ballData) {
-    }
+	public void setBallPositions(Ball[] ballData) {
+	}
 }
