920aab65b4221c4eadbdf8ff986a3944126a347f
matt.todd <matt.todd@asolutions.com>
2010-03-07 05:33:38 -0600

    re-organized the projects to be modules of a parent ball-game pom renamed the modules fixed a few tests

---
+++ ball-client/src/test/java/net/todd/games/balls/client/ui/MainModelPositionUpdateTest.java
@@ -0,0 +1,86 @@
+package net.todd.games.balls.client.ui;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import net.todd.games.balls.common.Ball;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainModelPositionUpdateTest {
+	private TestNetworkClient networkClient;
+
+	@Before
+	public void setUp() {
+		networkClient = new TestNetworkClient();
+	}
+
+	@Test
+	public void testMoveBallUp() {
+		MainModel model = new MainModel(null, networkClient);
+		assertNotNull(networkClient.ballSent);
+		assertEquals(0.0f, networkClient.ballSent.getPositionX());
+		assertEquals(0.0f, networkClient.ballSent.getPositionY());
+		assertEquals(0.0f, networkClient.ballSent.getPositionZ());
+		networkClient.ballSent = null;
+		model.moveBallUp();
+		assertNotNull(networkClient.ballSent);
+		assertEquals(0.0f, networkClient.ballSent.getPositionX());
+		assertEquals(0.0f, networkClient.ballSent.getPositionY());
+		assertEquals(1.0f, networkClient.ballSent.getPositionZ());
+	}
+
+	@Test
+	public void testMoveBallDown() {
+		MainModel model = new MainModel(null, networkClient);
+		assertNotNull(networkClient.ballSent);
+		assertEquals(0.0f, networkClient.ballSent.getPositionX());
+		assertEquals(0.0f, networkClient.ballSent.getPositionY());
+		assertEquals(0.0f, networkClient.ballSent.getPositionZ());
+		networkClient.ballSent = null;
+		model.moveBallDown();
+		assertNotNull(networkClient.ballSent);
+		assertEquals(0.0f, networkClient.ballSent.getPositionX());
+		assertEquals(0.0f, networkClient.ballSent.getPositionY());
+		assertEquals(-1.0f, networkClient.ballSent.getPositionZ());
+	}
+
+	@Test
+	public void testMoveBallLeft() {
+		MainModel model = new MainModel(null, networkClient);
+		assertNotNull(networkClient.ballSent);
+		assertEquals(0.0f, networkClient.ballSent.getPositionX());
+		assertEquals(0.0f, networkClient.ballSent.getPositionY());
+		assertEquals(0.0f, networkClient.ballSent.getPositionZ());
+		networkClient.ballSent = null;
+		model.moveBallLeft();
+		assertNotNull(networkClient.ballSent);
+		assertEquals(1.0f, networkClient.ballSent.getPositionX());
+		assertEquals(0.0f, networkClient.ballSent.getPositionY());
+		assertEquals(0.0f, networkClient.ballSent.getPositionZ());
+	}
+
+	@Test
+	public void testMoveBallRight() {
+		MainModel model = new MainModel(null, networkClient);
+		assertNotNull(networkClient.ballSent);
+		assertEquals(0.0f, networkClient.ballSent.getPositionX());
+		assertEquals(0.0f, networkClient.ballSent.getPositionY());
+		assertEquals(0.0f, networkClient.ballSent.getPositionZ());
+		networkClient.ballSent = null;
+		model.moveBallRight();
+		assertNotNull(networkClient.ballSent);
+		assertEquals(-1.0f, networkClient.ballSent.getPositionX());
+		assertEquals(0.0f, networkClient.ballSent.getPositionY());
+		assertEquals(0.0f, networkClient.ballSent.getPositionZ());
+	}
+
+	private static class TestNetworkClient extends AbstractNetworkClient {
+		private Ball ballSent;
+
+		@Override
+		public void update(Ball ball) {
+			ballSent = ball;
+		}
+	}
+}
---
+++ ball-common/src/test/java/net/todd/games/balls/common/BallCloneTest.java
@@ -0,0 +1,28 @@
+package net.todd.games.balls.common;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class BallCloneTest {
+	@Test
+	public void testClone() {
+		Ball ball1 = new Ball();
+		ball1.setId("123");
+		ball1.setColor(BallColor.Purple);
+		ball1.setPositionX(4);
+		ball1.setPositionY(5);
+		ball1.setPositionZ(6);
+
+		Ball ball2 = ball1.clone();
+		assertFalse(ball1 == ball2);
+		assertEquals("123", ball2.getId());
+		assertEquals(125, ball2.getColor().getColor().getRed());
+		assertEquals(0, ball2.getColor().getColor().getGreen());
+		assertEquals(125, ball2.getColor().getColor().getBlue());
+		assertEquals(4.0f, ball2.getPositionX());
+		assertEquals(5.0f, ball2.getPositionY());
+		assertEquals(6.0f, ball2.getPositionZ());
+	}
+}
